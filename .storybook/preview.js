5dfc75d13442ae23c6cd7298922a4ddb1e557341
Joshua Crass <joshua.crass@gmail.com>
2023-12-04 15:29:27 -0800

    updated theme provider and button

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,30 +1,5 @@
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-};
-
-export default preview;
-
 // /** @type { import('@storybook/react').Preview } */
-// import { ThemeProvider } from '../src/ThemeProvider';
-// import { defaultTheme } from '../src/theme';
-
-// const withThemeProvider = (Story, context) => (
-//   <ThemeProvider theme={defaultTheme}>
-//     <Story {...context} />
-//   </ThemeProvider>
-// );
-
 // const preview = {
-//   decorators: [withThemeProvider],
 //   parameters: {
 //     actions: { argTypesRegex: '^on[A-Z].*' },
 //     controls: {
@@ -37,3 +12,29 @@ export default preview;
 // };
 
 // export default preview;
+
+import { ThemeProvider } from '../src/ThemeProvider';
+import { defaultTheme } from '../src/theme';
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <ThemeProvider externalTheme={defaultTheme}>
+      <Story {...context} />
+    </ThemeProvider>
+  );
+};
+
+const preview = {
+  decorators: [withThemeProvider],
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
--- src/ThemeProvider.jsx
+++ src/ThemeProvider.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState } from 'react';
+import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
 import { defaultTheme } from './theme'; // Import defaultTheme
 
 // Create a new context called ThemeContext
@@ -11,28 +12,25 @@ export const useTheme = () => {
 
 // ThemeProvider component that provides the theme and a toggle function
 export const ThemeProvider = ({ children, externalTheme }) => {
-  // add test to validate the external theme is valid
+  // Add test to validate the external theme is valid
 
   const selectedTheme = externalTheme || defaultTheme;
-  // State to manage the current theme
-  const [theme, setTheme] = useState(selectedTheme.light);
+
   // State to track whether the dark theme is active
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
   // Function to toggle between light and dark themes
   const toggleTheme = () => {
     setIsDarkTheme((prevIsDarkTheme) => !prevIsDarkTheme);
-    setTheme((prevIsDarkTheme) =>
-      prevIsDarkTheme === selectedTheme.light
-        ? selectedTheme.dark
-        : selectedTheme.light
-    );
   };
+
+  // Determine the current theme based on the state
+  const theme = isDarkTheme ? selectedTheme.dark : selectedTheme.light;
 
   // Provide the theme and toggleTheme function to the components using ThemeContext
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
+      <EmotionThemeProvider theme={theme}>{children}</EmotionThemeProvider>
     </ThemeContext.Provider>
   );
 };
