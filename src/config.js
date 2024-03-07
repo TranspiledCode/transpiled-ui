5a679ba0c969b10cceb0e5b1f4521c296ad3a986
Joshua Crass <joshua.crass@gmail.com>
2024-03-07 10:18:48 -0800

    updated sizes and spacing on the bottom version

--- src/config.js
+++ src/config.js
@@ -1,40 +1,32 @@
 // Define common styles for input elements
 const commonStyles = {
   s: {
-    inputPadding: '4px 8px',
     inputFontSize: '1.8em',
     labelFontSize: '1.4em',
-    labelLeft: '0.6em',
+
     placeholderFontSize: '1.6em',
     placeholderTop: '0.5em',
-    placeholderLeft: '0.6em',
   },
   m: {
-    inputPadding: '6px 10px',
-    inputFontSize: '2em',
-    labelFontSize: '1.6em',
-    labelLeft: '0.5em',
+    inputFontSize: '1.8em',
+    labelFontSize: '1.2em',
+
     placeholderFontSize: '1.8em',
     placeholderTop: '0.5em',
-    placeholderLeft: '0.6em',
   },
   l: {
-    inputPadding: '8px 11px',
     inputFontSize: '2.2em',
     labelFontSize: '1.7em',
-    labelLeft: '0.6em',
+
     placeholderFontSize: '2em',
     placeholderTop: '0.7em',
-    placeholderLeft: '0.5em',
   },
   xl: {
-    inputPadding: '10px 11px',
     inputFontSize: '2.4em',
     labelFontSize: '1.8em',
-    labelLeft: '0.5em',
+
     placeholderFontSize: '2.1em',
     placeholderTop: '0.8em',
-    placeholderLeft: '0.5em',
   },
 };
 
@@ -43,18 +35,66 @@ const inputSizeVariants = {
   // Variants for the 'box' layout
   box: {
     ...commonStyles,
-    s: { ...commonStyles.s, labelTop: '-1.2em' },
-    m: { ...commonStyles.m, labelTop: '-1.2em' },
-    l: { ...commonStyles.l, labelTop: '-1.2em' },
-    xl: { ...commonStyles.xl, labelTop: '-1.2em' },
+    s: {
+      ...commonStyles.s,
+      labelTop: '-1.2em',
+      inputPadding: '4px 8px',
+      placeholderLeft: '0.6em',
+      labelLeft: '0.1em',
+    },
+    m: {
+      ...commonStyles.m,
+      labelTop: '-1.2em',
+      inputPadding: '6px 10px',
+      placeholderLeft: '0.6em',
+      labelLeft: '0.5em',
+    },
+    l: {
+      ...commonStyles.l,
+      labelTop: '-1.2em',
+      inputPadding: '8px 11px',
+      placeholderLeft: '0.5em',
+      labelLeft: '0.6em',
+    },
+    xl: {
+      ...commonStyles.xl,
+      labelTop: '-1.2em',
+      inputPadding: '10px 11px',
+      placeholderLeft: '0.5em',
+      labelLeft: '0.5em',
+    },
   },
   // Variants for the 'bottom' layout
   bottom: {
     ...commonStyles,
-    s: { ...commonStyles.s, labelTop: '2.3em', inputPadding: '4px 0px' },
-    m: { ...commonStyles.m, labelTop: '2.5em', inputPadding: '6px 0px' },
-    l: { ...commonStyles.l, labelTop: '2.8em', inputPadding: '8px 0px' },
-    xl: { ...commonStyles.xl, labelTop: '3.3em', inputPadding: '10px 0px' },
+    s: {
+      ...commonStyles.s,
+      labelTop: '2.3em',
+      inputPadding: '0.2em 0.1em',
+      placeholderLeft: '0.1em',
+      labelLeft: '0.1em',
+    },
+    m: {
+      ...commonStyles.m,
+      labelTop: '2.5em',
+      inputPadding: '0.2em 0.1em',
+      placeholderLeft: '0.1em',
+      labelLeft: '0.1em',
+    },
+    l: {
+      ...commonStyles.l,
+      labelTop: '2.8em',
+      inputPadding: '0.2em 0.1em',
+      placeholderLeft: '0.1em',
+      labelLeft: '0.1em',
+    },
+    xl: {
+      ...commonStyles.xl,
+      labelTop: '3.3em',
+      inputPadding: '0.2em 0.1em',
+      placeholderLeft: '0.1em',
+      labelLeft: '0.1em',
+    },
   },
 };
 
