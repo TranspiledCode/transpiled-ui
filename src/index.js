581efbb9709f91ece15cdcff2e9cbdeb166e5e50
Joshua Crass <joshua.crass@gmail.com>
2024-03-21 16:20:38 -0700

    release 37

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // index.js
-export { default as ScrollToTop } from '../components/ScrollToTop';
+export { default as ScrollToTop } from './components/ScrollToTop';
 export { default as useVisibility } from './hooks/useVisibility';
 export { default as Input } from './components/Input';
 export { default as Button } from './components/Button';
